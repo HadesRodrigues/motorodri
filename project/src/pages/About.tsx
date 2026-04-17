51dcaf7cdfb52162a7eb682f7835e4e182c4e05d
Joao Rodrigues <jeclimando2011@gmail.com>
2026-04-17 12:56:16 +0100

    mudança texto

--- project/src/pages/About.tsx
+++ project/src/pages/About.tsx
@@ -28,12 +28,12 @@ export default function About() {
             </h2>
             <p className="text-[#B8B8B8] text-lg leading-relaxed mb-4">
               Fundada com a missão de oferecer aos entusiastas automóveis uma experiência única
-              na aquisição de veículos premium, a MOTORODRI estabeleceu-se como referência no
-              mercado de automóveis de alta gama em Portugal.
+              na aquisição de Motas premium, a MOTORODRI estabeleceu-se como referência no
+              mercado de Motas de alta gama em Portugal.
             </p>
             <p className="text-[#B8B8B8] text-lg leading-relaxed mb-4">
-              Especializamo-nos na seleção criteriosa de veículos desportivos, clássicos e
-              premium, garantindo que cada motos no nosso stock passa por uma rigorosa
+              Especializamo-nos na seleção criteriosa de Motas desportivas, clássicas e
+              premium, garantindo que cada mota no nosso stock passa por uma rigorosa
               inspeção de qualidade e autenticidade.
             </p>
             <p className="text-[#B8B8B8] text-lg leading-relaxed">
